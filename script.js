a1fb200d0164fa667a8901226a5ae6034ecdaa93
Viviana-LT <vlinares@unsa.edu.pe>
2026-04-25 15:35:42 -0500

    funcion agregar

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function clean(){
     divResultado.innerHTML = "";
 }
 
+
+const btnAgregar = document.getElementById("agregar");
+
+let nombre = document.getElementById("nombre");
+const edad = document.getElementById("edad");
+const carrera = document.getElementById("carrera");
+const correo = document.getElementById("correo");
+
+const contentTabla = document.getElementById("contentTabla");
+
+//FUNCIÓN AGREGAR
+function agregar(){
+    contentTabla.innerHTML += `
+        <tr>
+            <td> ${nombre.value} </td>
+            <td> ${edad.value} </td>
+            <td> ${carrera.value} </td>
+            <td> ${correo.value} </td>
+        </tr>
+    `
+}
+
+btnAgregar.addEventListener("click", () => {
+    event.preventDefault(); 
+    agregar();
+});
