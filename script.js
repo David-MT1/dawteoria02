fbf1aeef6f905a5071a1cfdd79342625c9a4bafa
david <dmendozat@unsa.edu.pe>
2026-04-25 13:30:00 -0500

    commit de funcion clean

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//ESTA SERA LA CONSTANTE PARA MODIFICAR EL DIV-RESULTADO
+const divResultado = document.getElementById("resultado");
+
+
+
+//CLEAN DE DAVIID
+
+function clean(){
+    divResultado.innerHTML = "";
+}
+
